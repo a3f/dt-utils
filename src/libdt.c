a3f/dt-utils

commit 929ed64cb42f954648fb031af8e37e7282f4e4f3

of_get_devicepath: make partition finding more robust
If a backing device for a partition is neither an eeprom nor an mtd nor a
block device I don't know what it should be. Still fall back to the generic
case where the partition doesn't have a related of_node.

src/libdt.c
@@ -2324,13 +2324,14 @@ int of_get_devicepath(struct device_node *partition_node, char **devpath, off_t 
 			return udev_parse_eeprom(dev, devpath);
 		} else if (!udev_parse_mtd(dev, devpath, size)) {
 			return 0;
-		} else {
-			/* try to find a block device */
-			ret = device_find_block_device(dev, devpath);
-			if (ret)
-				return ret;
+		} else if (device_find_block_device(dev, devpath)) {
 			return of_parse_partition(partition_node, offset, size);
 		}
+
+		/*
+		 * If we found a device but couldn't classify it above, we fall
+		 * through.
+		 */
 	}
 
 	/*

commit e7d71f099659f6b95fc811db2df24c44f2d85899

libdt: fix of_get_devicepath looking up sibling if device unavailable
of_get_devicepath code flow is split into two:

  A) Either the device tree node in question has a direct udev_device
     associated with it

  B) Or we assume it's a partition and lookup udev_device for the parent
     first, before finding a child udev_device or setting a partition
     offset within the parent udev_device.

Since v2017.03.0, we have had a fallthrough from case A into case B:
If we have a udev_device, but it's neither a EEPROMs, MTDs or block
device, we just consider it a partition. This is problematic, because
this may result in us pointing at a very different device:

  - backend points at a SD-Card host. Host is enabled, but SD-Card
    is not inserted, so no block device

  - case A fails, so it's assumed it's a partition and case B
    uses parent SoC bus to lookup appropriate device

  - We fall through into the second device_find_block_device, which
    will take the first matching block device across the SoC. So
    we could end up with the eMMC: a completely different device
    than what was pointed at.

Fixes: 929ed64cb42f ("of_get_devicepath: make partition finding more robust")
Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Link: https://lore.pengutronix.de/20230607080818.715192-1-a.fatoum@pengutronix.de
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

src/libdt.c
@@ -2500,9 +2500,11 @@ int of_get_devicepath(struct device_node *partition_node, char **devpath, off_t 
 		}
 
 		/*
-		 * If we found a device but couldn't classify it above, we fall
-		 * through.
+		 * If we find a udev_device but couldn't classify it above,
+		 * it's an error. Falling through would mean to handle it as a
+		 * partition and could lead us to return an arbitrary sibling device
 		 */
+		return -ENODEV;
 	}
 
 	/*