a3f/dt-utils

commit 929ed64cb42f954648fb031af8e37e7282f4e4f3

of_get_devicepath: make partition finding more robust
If a backing device for a partition is neither an eeprom nor an mtd nor a
block device I don't know what it should be. Still fall back to the generic
case where the partition doesn't have a related of_node.

src/libdt.c
@@ -2324,13 +2324,14 @@ int of_get_devicepath(struct device_node *partition_node, char **devpath, off_t 
 			return udev_parse_eeprom(dev, devpath);
 		} else if (!udev_parse_mtd(dev, devpath, size)) {
 			return 0;
-		} else {
-			/* try to find a block device */
-			ret = device_find_block_device(dev, devpath);
-			if (ret)
-				return ret;
+		} else if (device_find_block_device(dev, devpath)) {
 			return of_parse_partition(partition_node, offset, size);
 		}
+
+		/*
+		 * If we found a device but couldn't classify it above, we fall
+		 * through.
+		 */
 	}
 
 	/*

commit 85cd795c6e82a73c0680cfd9d015646332e47392

libdt: drop broken if-branch
device_find_block_device returns 0 on success, so the way the else if
clause is now, only if there is a block device, the code falls through.
If there is none, a 0 is returned, but devpath is not populated breaking
the contract of the function. Just drop the branch for now and add it back
later in a way that works.

Fixes: 929ed64cb42f ("of_get_devicepath: make partition finding more robust")
Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
Link: https://lore.pengutronix.de/20230607121628.3180139-4-a.fatoum@pengutronix.de
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

src/libdt.c
@@ -2519,13 +2519,10 @@ int of_get_devicepath(struct device_node *partition_node, char **devpath, off_t 
 	 */
 	dev = of_find_device_by_node_path(partition_node->full_name);
 	if (dev) {
-		if (udev_device_is_eeprom(dev)) {
+		if (udev_device_is_eeprom(dev))
 			return udev_parse_eeprom(dev, devpath);
-		} else if (!udev_parse_mtd(dev, devpath, size)) {
+		if (!udev_parse_mtd(dev, devpath, size))
 			return 0;
-		} else if (device_find_block_device(dev, devpath)) {
-			return of_parse_partition(partition_node, offset, size);
-		}
 
 		/*
 		 * If we find a udev_device but couldn't classify it above,